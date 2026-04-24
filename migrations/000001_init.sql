8ba95a608536d97195db431a573e9707891608bf
kamilhajfullin <kamilhajfullin@gmail.com>
2026-04-24 19:04:35 +0500

    feat: add migrations and db connect

--- migrations/000001_init.sql
+++ migrations/000001_init.sql
@@ -7,8 +7,8 @@ CREATE TABLE IF NOT EXISTS subscriptions (
     start_date DATE NOT NULL,
     end_date DATE NOT NULL,
 
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	create_at TIMESTAMP DEFAULT NOW(),
+	update_at TIMESTAMP DEFAULT NOW(),
 );
 
 -- +goose Down

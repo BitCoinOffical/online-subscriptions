dc5f5bc9115922d23db85683d66f1595beb0af09
kamilhajfullin <kamilhajfullin@gmail.com>
2026-04-24 18:34:21 +0500

    feat: add logger

---
+++ pkg/logger.go
@@ -0,0 +1,32 @@
+package pkg
+
+import (
+	"errors"
+
+	"github.com/BitCoinOffical/online-subscriptions/config"
+	"go.uber.org/zap"
+)
+
+const (
+	EnvProd = "Prod"
+	EnvDev  = "Dev"
+)
+
+func NewLogger(cfg *config.AppConfig) (*zap.Logger, error) {
+	switch cfg.DebugLevel {
+	case "Dev":
+		logger, err := zap.NewDevelopment()
+		if err != nil {
+			return nil, err
+		}
+		return logger, nil
+	case "Prod":
+		logger, err := zap.NewDevelopment()
+		if err != nil {
+			return nil, err
+		}
+		return logger, nil
+	default:
+		return nil, errors.New("incorrect debug value")
+	}
+}

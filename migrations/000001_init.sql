3a67c89d95b06a7e59d9595ea27aaebfe8025aad
kamilhajfullin <kamilhajfullin@gmail.com>
2026-04-25 14:25:28 +0500

    fix(migrations): replace numeretic to integer

--- migrations/000001_init.sql
+++ migrations/000001_init.sql
@@ -2,7 +2,7 @@
 CREATE TABLE IF NOT EXISTS subscriptions (
     id BIGSERIAL PRIMARY KEY,
     service_name VARCHAR(255) NOT NULL,
-    price numeric(12, 2) NOT NULL,
+    price INTEGER NOT NULL,
     user_id UUID NOT NULL,
     start_date DATE NOT NULL,
     end_date DATE NOT NULL,
